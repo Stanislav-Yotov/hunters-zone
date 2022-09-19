785e1ce9e38e8877642e9cc74a4cf4578fa1e1fc
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-09-19 18:08:19 +0300

    finished with the embedded youtube player

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,17 +1,26 @@
 import styles from './Slider.module.css';
+import { useState } from 'react';
 
 function Slider() {
+    const [iframe, setIframe] = useState('https://www.youtube.com/embed/hQAHSlTtcmY?enablejsapi=1');
+
+    function frameHandler() {
+        if (iframe === 'https://www.youtube.com/embed/hQAHSlTtcmY?enablejsapi=1') {
+            setIframe('https://www.youtube.com/embed/dGcsHMXbSOA?enablejsapi=1');
+        } else if(iframe === 'https://www.youtube.com/embed/dGcsHMXbSOA?enablejsapi=1') {
+            setIframe('https://www.youtube.com/embed/hQAHSlTtcmY?enablejsapi=1')
+        }
+    }
+
+    
     return (
-        <div className={styles.videoSlider}>
+        <div className={styles.videoSlider} id='slider-video'>
             <div className={styles.slide}>
-                <iframe className={styles.video} src='https://www.youtube.com/embed/hQAHSlTtcmY?enablejsapi=1' title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-            </div>
-            <div className={styles.slide}>
-                <iframe className={styles.video} src='https://www.youtube.com/embed/dGcsHMXbSOA?enablejsapi=1' title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                <iframe className={styles.video} id="ytplayer" type="text/html" width="640" height="360" src={iframe} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" loop={true} autoPlay={true} allowFullScreen></iframe>
             </div>
             <div className={styles['slide-arrow']}>
-                <div className={styles.left} />
-                <div className={styles.right} />
+                <div className={styles['slide-arrow-left']} onClick={frameHandler} />
+                <div className={styles['slide-arrow-right']} onClick={frameHandler} />
             </div>
         </div>
     );
